341ed9973815625e81410213c3d79821fb605a59
devcom2020 <kwp@tlen.pl>
2022-09-14 16:03:20 +0200

    środa 16:03

---
+++ dane.py
@@ -0,0 +1,14 @@
+# wartość None
+
+zmienna = None
+
+# uzyskiwanie danych od użytkownika
+#
+# imie = input("Podaj swoje imię: ")
+# print("Twoje imię to:", imie)
+
+print("- " * 10, "Witaj w kalkulatorze który tylko dodaje!", "- " * 10)
+liczba1 = int(input("Podaj pierwszą liczbę:"))
+liczba2 = int(input("Podaj drugą liczbę:"))
+wynik = liczba1 + liczba2
+print("Wynik=", wynik)
---
+++ pętla1.py
@@ -0,0 +1,11 @@
+# while (dopóki) warunek:
+#   ciało pętli
+# else:
+#   kod który, gdy warunek nie jest spełniony
+
+licznik = 0
+while licznik < 5:
+    print(licznik, "mniejszy od 5")
+    # licznik = licznik + 1
+    licznik += 1
+print("Poza pętlą!")
