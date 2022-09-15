e6c8235f6b8352941615e1ae9a09903d6b92c22c
devcom2020 <kwp@tlen.pl>
2022-09-15 10:00:10 +0200

    czwartek 10:00

--- pętla1.py
+++ pętla1.py
@@ -8,4 +8,22 @@ while licznik < 5:
     print(licznik, "mniejszy od 5")
     # licznik = licznik + 1
     licznik += 1
-print("Poza pętlą!")
+    if licznik == 3:
+        break
+# print("Poza pętlą!")
+print(licznik)
+
+
+
+
+if licznik == 5:
+    print(licznik, "równe 5")
+elif licznik > 6:
+    print(licznik, "większe od 6")
+    if licznik == 10:
+        print("Licznik jest 10...")
+elif licznik < 4:
+    print(licznik, "w instrukcji if mniejsze od 4")
+else:
+    print("Ja tu sobie jestem else...")
+
